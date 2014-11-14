3fa353ab5cb97c5347a0c831afde01ffb2f0b774
Roy Kim <rykim79@gmail.com>
2014-11-13 21:05:30 -0800

    v2

--- js/myApp.js
+++ js/myApp.js
@@ -1,4 +1,27 @@
 angular.module('myApp', [])
-	.controller('MultiplicationCtrl', function($scope){
-		$scope.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	.controller('MultiplicationCtrl', function($scope, $attrs) {
+		// write a function that will populate the numbers
+		function populateNumbers(x) {
+			// create a var to store numbers in an array
+			var numbers = [];
+
+			// run a "for" loop from 0 - x that creates the numbers
+			for (var i = 0; i < x; i++) {
+				numbers[i] = i + 1;
+			};
+
+			// return the numbers
+			return numbers;
+		}
+
+		$scope.compute = function(a, b) {
+			return a * b;
+		};
+		
+		$scope.$watch('numberLimit', function(limit){
+			$scope.numbers = populateNumbers(limit);
+		});
+
+		// SET numberLimit to the initialNumberLimit OR 10
+		$scope.numberLimit = $attrs.initialNumbersLimit || 10;
 	});
